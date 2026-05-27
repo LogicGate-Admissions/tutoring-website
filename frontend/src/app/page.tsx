7cf6a19fe305a40f2f591a56115f1318fbd87f6a
Tasnim Uddin Syed <tasnim.syed06@gmail.com>
2026-05-27 01:43:55 +0100

    test(cd): verify vercel deployment new app [tus24]

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="min-h-screen p-8">
       <h1 className="text-3xl font-bold">Tutoring Website</h1>
-      <p className="mt-4">Vercel deployment is working.</p>
+      <p className="mt-4">Vercel deployment is working HEY ARNAV!!!!!!!!!!!!.</p>
     </main>
   );
 }
